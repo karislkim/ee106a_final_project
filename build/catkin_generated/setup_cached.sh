1415762c481898c7fa94bc15f70570aecc6eca54
karis <klkim@berkeley.edu>
2024-11-14 18:34:55 -0800

    struggling w turtlebot

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/cc/ee106a/fa24/class/ee106a-afh/ros_workspaces/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/cc/ee106a/fa24/class/ee106a-afh/ros_workspaces/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/home/cc/ee106a/fa24/class/ee106a-afh/ros_workspaces/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/cc/ee106a/fa24/class/ee106a-afh/ros_workspaces/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/cc/ee106a/fa24/class/ee106a-afh/ros_workspaces/src:$ROS_PACKAGE_PATH"
